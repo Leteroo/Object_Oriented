2be22d24d21273a99ddce06a0f3b92ffbdb7be7c
Leteroo <stars22375@gmail.com>
2022-06-02 23:15:02 +0800

    new

--- practice_2.py
+++ practice_2.py
@@ -18,56 +18,51 @@ import sys
 import time
 
 class TURTLE:
-    # 烏龜初始狀態
     def __init__(self):
         self.power = 100
         self.x = random.randint(0, 10)
         self.y = random.randint(0, 10)
-    # 烏龜移動的方法
+
     def move(self):
-        # 選擇這次移動的距離
         self.new_x = self.x + random.choice([1, 2, -1, -2])
         self.new_y = self.y + random.choice([1, 2, -1, -2])
-        # 判斷x座標移動後的位置
+
         if self.new_x < 0:
             self.x = 0 - self.new_x
         elif self.new_x > 10:
             self.x = 10 - (self.new_x - 10)
         else:
             self.x = self.new_x
-        # 判斷y座標移動後的位置
+
         if self.new_y < 0:
             self.y = 0 - self.new_y
         elif self.new_y > 10:
             self.y = 10 - (self.new_y - 10)
         else:
             self.y = self.new_y
-        # 移動一次體力-1
+
         self.power -= 1
-    # 吃掉魚
+
     def eat(self):
         self.power += 20
         if self.power > 100:
             self.power = 100
 class FISH:
-    # 魚初始狀態
     def __init__(self):
         self.x = random.randint(0, 10)
         self.y = random.randint(0, 10)
 
-    # 魚這次移動的方法
     def move(self):
-        # 選擇這次移動的距離
         self.new_x = self.x + random.choice([1, -1])
         self.new_y = self.y + random.choice([1, -1])
-        # 判斷x座標移動後的位置
+
         if self.new_x < 0:
             self.x = 0 - self.new_x
         elif self.new_x > 10:
             self.x = 10 - (self.new_x - 10)
         else:
             self.x = self.new_x
-        # 判斷y座標移動後的位置
+
         if self.new_y < 0:
             self.y = 0 - self.new_y
         elif self.new_y > 10:
@@ -77,15 +72,13 @@ class FISH:
 
 # 進行遊戲
 def startgame():
-    # 開始前生成烏龜和魚
     turtle = TURTLE()
     print("烏龜初始位置為:({0},{1})".format(turtle.x, turtle.y))
     fish = []
     for i in range(10):
         fish.append(FISH())
-        print("第{0}隻魚的初始位置為:({1},{2})".format(i + 1, fish[i].x, fish[i].y))
-    print()
-    #遊戲開始
+        print("第{0}隻魚的初始位置為:({1},{2})\n".format(i + 1, fish[i].x, fish[i].y))
+
     while True:
         if turtle.power <= 0:
             print("烏龜體力為0死亡，魚獲勝\n\n\n")
@@ -104,9 +97,9 @@ def startgame():
                     print("死了一隻魚，位置在({0},{1})".format(turtle.x, turtle.y))
                     print("烏龜剩餘體力為{0}".format(turtle.power))
                     print("剩下{0}隻魚\n".format(len(fish)))
-# 是否結束程式
+
 while True:
-    key = input("輸入q關閉程式 或 隨便輸入開始遊戲:")
+    key = input("輸入q關閉程式 或 輸入任意字符開始遊戲:")
     if key.lower().startswith('q') == 1:
         print("3秒後自動關閉程式")
         time.sleep(3)
