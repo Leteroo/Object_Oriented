5220bfc46b963ae1765d48fb02d78a0d1b366002
Leteroo <stars22375@gmail.com>
2022-06-11 14:20:15 +0800

    new

--- OO_practice_1.py
+++ OO_practice_1.py
@@ -1,49 +1,36 @@
-# 房貸月繳金額計算
-import sys
-
-class homeloan:
-    def __init__(self, name, years, amount, rate):
+# 會員制度
+class members:
+    def __init__(self, cost, name):
         self.__name = name
-        self.__years = years
-        self.__amount = amount
-        self.__rate = rate
+        self.__cost = cost
     def getname(self):
         return self.__name
-    def get_month_payment(self):
-        return round((self.__amount * (1 + self.__rate)) / (self.__years * 12))
+    def checkout(self):
+        return self.__cost
+    def printcheckout(self):
+        print('Hello {0}! How do you do today!'.format(self.getname()))
+        print('All things you buy are {0} dollars.'.format(self.__cost))
+    
+class silvermembers(members):
+    def __checkout(self):
+        return round(super().checkout() * 0.95)
+    def printcheckout(self):
+        print('Glad to see you {0}! We have some discount for you!'.format(super().getname()))
+        print('All things you buy are {0} dollars. See you next time!'.format(self.__checkout()))
 
-while True:
-    print("輸入Q離開或隨意鍵繼續")
-    key = input()
-    key.lower()
-    if key == 'q':
-        sys.exit()
-    while True:
-        name = input("姓名:")
-        while True:
-            try:
-                amount = eval(input("貸款總金額(單位:萬):"))
-                amount *= 10000
-            except NameError:
-                print("輸入金額格式錯誤，請重新輸入")
-            except SyntaxError:
-                print("輸入金額格式錯誤，請重新輸入")
-            else:
-                break
-        while True:
-            years = eval(input("貸款年限(3,5,20):"))
-            if years != 3 and years != 5 and years != 20:
-                print("貸款年限格式錯誤，請重新輸入")
-                continue
-            elif years == 3:
-                rate = 0.0603
-                break
-            elif years == 5:
-                rate = 0.0620
-                break
-            elif years == 20:
-                rate = 0.0650
-                break
-        client = homeloan(name, years, amount, rate)
-        print("{0}的每月還款金額為{1:,}元\n".format(client.getname(), client.get_month_payment()))
-        break
+class goldenmembers(members):
+    def __checkout(self):
+        return round(super().checkout() * 0.85)
+    def printcheckout(self):
+        print('Welcome back {0}! How can we help you today?'.format(super().getname()))
+        print('All things you buy are {0} dollars. We are always ready to serve you!'.format(self.__checkout()))
+
+customer1 = members(30000, 'Ran')
+customer2 = silvermembers(30000, 'Michelle')
+customer3 = goldenmembers(30000, 'Isaac')
+
+customer1.printcheckout()
+print()
+customer2.printcheckout()
+print()
+customer3.printcheckout()
